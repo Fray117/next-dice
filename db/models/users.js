28d1d969ad710cfa4b5356abea26fe2821c72505
Fray117 <5698303-Fray117@users.noreply.gitlab.com>
2022-06-01 14:21:24 +0700

    db update

--- db/models/users.js
+++ db/models/users.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
 		 * The `models/index` file will call this method automatically.
 		 */
 		static associate(models) {
-			// define association here
+			this.hasMany(models.room_master, { as: 'rooms' })
 		}
 	}
 	users.init({
